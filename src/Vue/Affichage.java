adcacab3cc523564569abe96dde74c275e964ee5
NielsTRS <trsniels@gmail.com>
2024-04-18 19:10:01 +0200

    Revert "Condition fin partie"
    This reverts commit 830809ba

--- src/Vue/Affichage.java
+++ src/Vue/Affichage.java
@@ -10,27 +10,78 @@ import java.io.FileInputStream;
 
 
 public class Affichage extends JComponent {
-
+    Image gauffreEmpoisonne = charge("res/Images/gauffre_empoisonne.png");
+    Image gauffreNormal = charge("res/Images/gauffre.png");
     private Grille grille;
-    private ZoneGauffre gauffre;
 
     //TODO : faire classe zone avec les tailles des zones
-
+    int largeurCase;
+    int hauteurCase;
     int largeurFenetre;
     int hauteurFenetre;
+    int largeurGauffre;
+    int hauteurGauffre;
+
     public Affichage(Grille grille) {
-        this.gauffre = new ZoneGauffre(grille);
+        this.grille = grille;
+    }
+
+    private void paintGauffre(Graphics g){
+        largeurCase = largeurGauffre / grille.getColonnes();
+        hauteurCase = hauteurGauffre / grille.getLignes();
+
+        //TODO: faire condition de fin de partie
+        /*if(false){
+            g.drawString("Fin de la partie", getWidth() / 2, getHeight() / 2);
+        } else {*/
+        for (Case c : grille.getCases()) {
+            if(c.getEstMange()) {
+                continue; // Passe à l'itération suivante si la case est déjà mangée
+            }
+
+            if(c.getEstPoisson()) {
+                g.drawImage(gauffreEmpoisonne, c.getX()*largeurCase, c.getY()*hauteurCase, largeurCase, hauteurCase, null);
+            } else {
+                g.drawImage(gauffreNormal, c.getX()*largeurCase, c.getY()*hauteurCase, largeurCase, hauteurCase, null);
+            }
+            // TODO : changer en var de case dans
+        }
+        //}
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         largeurFenetre = getSize().width;
-
+        largeurGauffre = largeurFenetre - largeurFenetre/3;
         hauteurFenetre = getSize().height;
-
-        gauffre.paintGauffre(g, largeurFenetre, hauteurFenetre);
+        hauteurGauffre = hauteurFenetre;
+        paintGauffre(g);
 
     }
 
+    public Image charge(String nom) {
+        try {
+            return ImageIO.read(new FileInputStream(nom));
+        } catch (Exception e) {
+            System.err.println("Impossible de charger l'image " + nom);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    public int getHauteurGrille(){
+        return hauteurGauffre;
+    }
+    public int getLargeurGrille(){
+        return largeurGauffre;
+    }
+
+    public int getHauteurCase() {
+        return hauteurCase;
+    }
+
+    public int getLargeurCase() {
+        return largeurCase;
+    }
 
 }
