97bff2e9c01d292e53b88c73bdf5484896fef3b3
ptiboo <thibault.poncon@gmail.com>
2024-04-18 15:45:22 +0200

    premiere version de affichage

--- src/Vue/Affichage.java
+++ src/Vue/Affichage.java
@@ -1,6 +1,8 @@
 package Vue;
 
 import Modele.Caisse;
+import Modele.Case;
+import Modele.Grille;
 import Modele.Niveau;
 
 import javax.imageio.ImageIO;
@@ -9,22 +11,40 @@ import java.awt.*;
 import java.io.FileInputStream;
 
 public class Affichage extends JComponent {
-    Image imagePousseur = charge("res/Images/gauffre_empoisonne.png");
-    Image imageCaisse = charge("res/Images/gauffre.png");
-    private Niveau niveau;
+    Image gauffreEmpoisonne = charge("res/Images/gauffre_empoisonne.png");
+    Image gauffreNormal = charge("res/Images/gauffre.png");
+    private Grille grille;
+    int largeurCase;
+    int hauteurCase;
 
-    public Affichage(Niveau niveau) {
-        this.niveau = niveau;
+    public Affichage(Grille grille) {
+        this.grille = grille;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        if(niveau.collision()){
+        int largeur = getSize().width;
+        int hauteur = getSize().height;
+        largeurCase = largeur / grille.getColonnes();
+        hauteurCase = hauteur / grille.getLignes();
+
+        //TODO: faire condition de fin de partie
+        if(false){
             g.drawString("Fin de la partie", getWidth() / 2, getHeight() / 2);
         } else {
-            g.drawImage(imagePousseur, niveau.getPersonnage().getX(), niveau.getPersonnage().getY(), null);
-            for (Caisse caisse : niveau.getCaisses()) {
-                g.drawImage(imageCaisse, caisse.getX(), caisse.getY(), null);
+            for (Case c : grille.getCases()) {
+                switch (c.getEtat()){
+                    //TODO : changer en var de case dans
+                    case 0:
+                        g.drawImage(gauffreNormal, c.getX(), c.getY(), largeurCase, hauteurCase, null);
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        g.drawImage(gauffreNormal, c.getX(), c.getY(), largeurCase, hauteurCase, null);
+
+                }
+
             }
         }
     }
